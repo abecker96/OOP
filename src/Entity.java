6f8e1681b9ee44e4c852106463c949b2a8f5dbac
Aidan Becker <49326603+abecker96@users.noreply.github.com>
2020-04-08 16:41:41 -0500

    Finishing basic skeleton
    These and all commits before this are all part of creating the basic skeleton of the project, only creating the filesystem, Entity, Room classes.

---
+++ src/Entity.java
@@ -0,0 +1,12 @@
+
+public class Entity 
+{
+	private String name;
+	private Room place;
+	
+	public Entity(String name, Room place)
+	{
+		this.name = name;
+		this.place = place;
+	}
+}
---
+++ src/Room.java
@@ -0,0 +1,13 @@
+
+public class Room 
+{
+	private int roomNum;
+	private int[] entities;
+	
+	public Room(int n)
+	{
+		roomNum = n;
+	}
+	
+	
+}
